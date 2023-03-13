b515393ec418ffc318c2c6033a92a3c1523a3a9b
Lisa <mkoliju@gmail.com>
2023-03-13 09:35:27 -0700

    Navbar button page change functional

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Navbar(){
+    const navigate = useNavigate()
+
+    const handleClickToShop = () => {
+        navigate('/shop')
+    }
+
+    const handleClickToHome = () => {
+        navigate('/')
+    }
+
+    const handleClickToCart = () => {
+        navigate('/cart')
+    }
+
     return(
         <div className='sticky top-0'>
             <ul className="flex">
                 <li className="flex-1 mr-2">
-                    <div className="text-center block py-2 px-4 hover:text-blue-800" >CatCatShop</div>
+                    <button 
+                        className="text-center block py-2 px-4 hover:text-blue-800"
+                        onClick={handleClickToHome}
+                    >CatCatShop</button>
                 </li>
                 <li class="flex-1 mr-2">
-                    <div className="text-center block py-2 px-4 text-blue-500 hover:text-blue-800">Shop</div>
+                    <button 
+                        className="text-center block py-2 px-4 text-blue-500 hover:text-blue-800" 
+                        onClick={handleClickToShop}
+                    >Shop</button>
                 </li>
                 <li class="flex-1 mr-2">
-                    <div className="text-center block py-2 px-4 text-blue-500 hover:text-blue-800">Cart</div>
+                    <button 
+                        className="text-center block py-2 px-4 text-blue-500 hover:text-blue-800"
+                        onClick={handleClickToCart}
+                    >Cart</button>
                 </li>
 
             </ul>
