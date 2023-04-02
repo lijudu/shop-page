76011dec7f011450f66b7ffc59c1060e299b2345
Lisa <mkoliju@gmail.com>
2023-04-01 19:32:42 -0700

    HashRouter

--- src/Components/RouteSwitch.js
+++ src/Components/RouteSwitch.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Shop from './Shop'
 import Cart from './Cart'
@@ -9,14 +9,14 @@ import { ShopContextProvider } from './Context'
 export default function RouteSwitch(){
     return(
         <ShopContextProvider>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path='/' element={<Home/>} />
                     <Route path='/shop' element={<Shop/>}/>
                     <Route path='/cart' element={<Cart/>}/>
                     <Route path='/item' element={<ItemPurchase/>}/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </ShopContextProvider>
     )
 }
